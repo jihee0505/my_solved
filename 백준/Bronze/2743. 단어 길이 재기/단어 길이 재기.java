6e75b937d51ac45ee1192dd3a2e6b483837a7f45
jihee0505 <choijihee1515@gmail.com>
2025-09-06 21:42:57 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 168 ms, Memory: 17544 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		//문자열 입력
+		String str = sc.next();
+		
+		//길이 출력
+		System.out.println(str.length());
+	}
+
+}
