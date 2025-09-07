320a57496c2f2a56d9f11cb351a8a7f238f534f9
jihee0505 <choijihee1515@gmail.com>
2025-09-07 20:37:14 +0900

    [Bronze V] Title: 새싹, Time: 100 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//따옴표와 역슬래시 주의하기
+		//따옴표는 \"
+		//역슬래시는 \\
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+		
+		
+	}
+
+}
