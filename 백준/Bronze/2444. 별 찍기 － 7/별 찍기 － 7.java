aa11b4ec09f9f1ee303b4de901a6c83e825c45f7
jihee0505 <choijihee1515@gmail.com>
2025-09-07 21:25:43 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 496 ms, Memory: 24868 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		//절반 위 삼각형
+		//i가 증가하면 별 개수도 증가함
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n-i; j++) {
+				System.out.print(" ");
+			}
+			for(int j=1; j<=2*i-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		//절반 아래 삼각형
+		//i가 감소하면 별 개수도 감소함
+		for(int i=n-1; i>=1; i--) {
+			for(int j=1; j<=n-i; j++) {
+				System.out.print(" ");
+			}
+			for(int j=1; j<=2*i-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	
+	}
+
+}
